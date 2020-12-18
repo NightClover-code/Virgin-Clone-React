960aa1d28421ce04dbd5cf38b090069c1d8fa0f0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2020-12-18 01:30:53 +0100

    added text toggling on menu logo click

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,22 @@ import './css/app.css';
 
 import AdditionalInfo from './components/AdditionalInfo';
 import Footer from './components/Footer';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Scoop from './components/Scoop/Scoop';
 import Showcase from './components/Showcase';
 import SocialMedia from './components/SocialMedia';
-//TODO refactor scoop
 //importing components
 import VirginCompanies from './components/VirginCompanies/VirginCompanies';
 import { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  //
-  const dropDowns = [];
-  //getting the dropDown ref
-  const getRef = listsRef => {
-    setTimeout(() => {
-      dropDowns.push(listsRef.current);
-    }, 20);
-  };
-  //hover dropdown functions
-  const onItemHover = target => {
-    dropDowns.forEach(dropDown => {
-      if (
-        dropDown.getAttribute('id') ===
-        target.getAttribute('data-id')
-      ) {
-        dropDown.classList.add('active');
-      } else {
-        dropDown.classList.remove('active');
-      }
-    });
-  };
   return (
     <div className="app-container">
       <Header />
       <NavBar />
-      <Showcase getRef={ref => getRef(ref)} />
+      <Showcase />
       <VirginCompanies
         counter={counter}
         setCounter={setCounter}
---
+++ src/components/Header/MenuDropDown.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MenuDropDown = () => {
+  return <div className="menu-drop-down"></div>;
+};
+
+export default MenuDropDown;
